b42aedd68b8a6533af69c7f2e7ef4a3c40886f2e
ali <khakbazan.io@proton.me>
2025-03-13 22:46:19 +0330

    chore: updating plugin build and zip file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '4c1bceb7389534d8c8d6');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'cd614994fad3b46527f9');
